778731444ffdeb2d78a547d0535b64fdf7b60c8a
Moiseszs <48396278+Moiseszs@users.noreply.github.com>
2024-01-21 01:50:56 -0300

    feat: add curves

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,13 +49,19 @@ function fillDetails(pokemon){
                 <img src="${pokemon.photo}">
             </div> 
         </div>
+        <div style="margin-bottom: -320px; background-color: white">
+            <div class="${pokemon.type}" style="width: 7%; height: 100px;border-bottom-right-radius: 0.40%; "> </div>
+            <div style="background-color: #f6f8fc; transform: translate(-1px, -100px);  width: 10%; height: 100px; border-top-left-radius: 60%; padding: 10px; "></div>
+            <div class="${pokemon.type}" style="transform: translate(90vw, -200px);  width: 10%; height: 100px; border-bottom-left-radius: 89%; padding: 1px;"></div>
+            <div style="overflow: hidden;background-color: inherit;transform: translate(90vw, -300px);  width: 10%; height: 100px; border-top-right-radius: 80%; padding-left: 100px;"></div>
+        </div>
         <div class="info" style="background-color: white; color: #292b29; padding: 1rem">
             <div>
                 <h3>About: </h3>
                 <div style="padding-left: 10px;">
                     <p>Height: ${pokemon.height / 10} cm</p>
                     <p>Weight: ${pokemon.weight} lb </p>
-                    <p>Abilities: ${pokemon.types.map((ability) => `${ability}`)}
+                    <p>Abilities: ${pokemon.types.map((ability) => ` ${ability}`)}
                 </div>
                 
             </div>
